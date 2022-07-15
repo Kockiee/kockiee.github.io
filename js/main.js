17df99e52e3523e63a9a83f7b4441c640e60a104
Miguel Eduardo <98851408+Kockiee@users.noreply.github.com>
2022-07-15 15:25:42 -0300

    a

---
+++ js/main.js
@@ -0,0 +1,4 @@
+function Download() {
+    window.open("https://github.com/Kockiee/KockieOptimizer/archive/refs/heads/master.zip", "_blank")
+    window.location.href = "templates/successfulDownload.html"
+}
