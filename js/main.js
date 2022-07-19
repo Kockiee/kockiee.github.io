dd1424416927c3fe0f1595e4f39fdad000bacc68
Miguel Eduardo <98851408+Kockiee@users.noreply.github.com>
2022-07-19 09:38:09 -0300

    Updated to version 1.0.5

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 function Download() {
     window.open("https://github.com/Kockiee/KockieOptimizer/archive/refs/heads/master.zip", "_blank")
-    window.location.href = "templates/successfulDownload.html"
+    window.location.href = "templates/successfulDownload.html";
 }
