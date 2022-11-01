0976240a2695c3b2f532294f3db34a10e76b27cd
Miguel Eduardo <98851408+Kockiee@users.noreply.github.com>
2022-11-01 13:15:58 -0300

    C# add

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 function download1(){
     window.open("https://github.com/Kockiee/KockieOptimizer/archive/refs/heads/Version-0.7.3.zip");
 }
+function download2(){
+    window.open("https://github.com/Kockiee/Kockie-Optimizer-C-Sharp/releases/download/V-1.0.0/Kockie.Optimizer-x64.64bit.zip");
+}
+function download3(){
+    window.open("https://github.com/Kockiee/Kockie-Optimizer-C-Sharp/releases/download/V-1.0.0/Kockie.Optimizer-x86.32bit.zip");
+}
 function opendownloadpage(){
     window.location.href = "templates/download.html"
 }
+
