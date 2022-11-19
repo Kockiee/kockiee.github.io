1db776b405205157df599f31631484abb9df63f5
Miguel Eduardo <98851408+Kockiee@users.noreply.github.com>
2022-11-19 19:33:53 -0300

    Big Update
    Images was added and modification on download links was doing

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 function download1(){
-    window.open("https://github.com/Kockiee/KockieOptimizer/archive/refs/heads/Version-0.7.3.zip");
+    window.open("https://github.com/Kockiee/KockieOptimizer/archive/refs/heads/Version-0.8.zip");
 }
 function download2(){
-    window.open("https://github.com/Kockiee/Kockie-Optimizer-C-Sharp/releases/download/V-1.1.0/Kockie.Optimizer.-.x64.64bit.zip");
+    window.open("https://github.com/Kockiee/Kockie-Optimizer-C-Sharp/releases/download/V-1.2.0/Kockie.Optimizer.-.x64.64bit.zip");
 }
 function download3(){
-    window.open("https://github.com/Kockiee/Kockie-Optimizer-C-Sharp/releases/download/V-1.1.0/Kockie.Optimizer.-.x86.32bit.zip");
+    window.open("https://github.com/Kockiee/Kockie-Optimizer-C-Sharp/releases/download/V-1.2.0/Kockie.Optimizer.-.x86.32bit.zip");
 }
 function opendownloadpage(){
-    window.location.href = "templates/download.html"
+    window.location.href = "templates/download.html";
 }
-
