df7d6dd979cb9a9ce07f263aa25a7e9e3e7876e8
Miguel Eduardo <98851408+Kockiee@users.noreply.github.com>
2022-11-30 15:03:30 -0300

    Update links

--- js/main.js
+++ js/main.js
@@ -2,11 +2,24 @@ function download1(){
     window.open("https://github.com/Kockiee/KockieOptimizer/archive/refs/heads/Version-0.8.zip");
 }
 function download2(){
-    window.open("https://github.com/Kockiee/Kockie-Optimizer-C-Sharp/releases/download/V-1.2.0/Kockie.Optimizer.-.x64.64bit.zip");
+    window.open("https://github.com/Kockiee/Kockie-Optimizer-C-Sharp/releases/download/V-1.3.0/Kockie.Optimizer.-.x64.64bit.zip");
 }
 function download3(){
-    window.open("https://github.com/Kockiee/Kockie-Optimizer-C-Sharp/releases/download/V-1.2.0/Kockie.Optimizer.-.x84.32bit.zip");
+    window.open("https://github.com/Kockiee/Kockie-Optimizer-C-Sharp/releases/download/V-1.3.0/Kockie.Optimizer.-.x84.32bit.zip");
 }
 function opendownloadpage(){
     window.location.href = "templates/download.html";
 }
+
+window.onload = function() {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+    {
+        var adsDivs = document.getElementsByClassName('Ads');
+        var vLines = document.getElementsByClassName('verticalLines');
+        var pgmDiv = document.getElementsByClassName('programa3');
+
+        window.document.removeChild(adsDivs);
+        vLines.style.transform = "rotate(90deg)";
+        pgmDiv.style.transform = "flex-direction: column";
+    }
+} 
